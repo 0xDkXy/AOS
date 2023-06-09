0fc91f6b0a6c2a5effdf3a4e6b0a27d7f0fc0741
0xDkXy <bo001015@outlook.com>
2023-06-09 21:49:28 +0800

    feat: improvement of interrupt complete the part of 7.6.2 improve the interrupt, every single number will have its own handler

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -22,6 +22,13 @@ struct gate_desc
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function);
 static struct gate_desc idt[IDT_DESC_CNT];
 
+char* intr_name[IDT_DESC_CNT]; // restore the name of interruption
+// define the array of interrupt handler, which is defined in kernel.S
+// Just the entry of interrupt handler function.
+// Will call the processing function in the ide_table finally
+intr_handler idt_table[IDT_DESC_CNT];
+
+// declare the entrys array of interrupt handle function defining in kernel.S
 extern intr_handler intr_entry_table[IDT_DESC_CNT];
 
 static void make_idt_desc(struct gate_desc* p_gdesc, uint8_t attr, intr_handler function)
@@ -63,10 +70,69 @@ static void pic_init(void)
     put_str("   pic_init done\n");
 }
 
+/* general interrupt handler function
+ * call it usually when the exception occure
+ * */
+static void general_intr_handler(uint8_t vec_nr)
+{
+    // The spurious interrupt made by IRQ7 and IRQ15, no need to handle it.
+    // 0x2f is the last IRQ pin of 8259A, keep.
+    if (vec_nr == 0x27 || vec_nr == 0x2f)
+    {
+        return;
+    }
+    put_str("int vector : 0x");
+    put_int(vec_nr);
+    put_char('\n');
+}
+
+/*
+ * complete registing of general interrupt handler 
+ * and the name of exception.
+ * */
+static void exception_init(void)
+{
+    int i;
+    for (i = 0; i < IDT_DESC_CNT; i++)
+    {
+        // default, will be registed by resiter_handler later.
+        idt_table[i] = general_intr_handler; 
+        intr_name[i] = "unknown"; // default name
+    }
+    intr_name[0] = "#DE Divide Error";
+    intr_name[1] = "#DB Debug Exception";
+    intr_name[2] = "NMI Interrupt";
+    intr_name[3] = "#BP Breakpoint Exception";
+    intr_name[4] = "#OF Overflow Exception";
+    intr_name[5] = "#BR Bound Range Exceeded Exception";
+    intr_name[6] = "#UD Invalid Opcode Exception";
+    intr_name[7] = "#NM Device Not Available Exception";
+    intr_name[8] = "#DF Double Fault Exception";
+    intr_name[9] = "Coprocessor Segment Overrun";
+    intr_name[10] = "#TS Invalid TSS Exception";
+    intr_name[11] = "#NP Segment Not Present";
+    intr_name[12] = "#SS Stack Fault Exception";
+    intr_name[13] = "#GP General Protection Exception";
+    intr_name[14] = "#PF Page-Fault Exception";
+    // intr_name[15] = ""; // keep for intel
+    intr_name[16] = "#MF x87 FPU Floating-Point Error";
+    intr_name[17] = "#AC Alignment Check Exception";
+    intr_name[18] = "#MC Machine-Check Exception";
+    intr_name[19] = "#XF SIMD Floating-Point Exception";
+}
+
+
+/*
+ * complete all the initializing work before off the interrupt.
+ * */
 void idt_init()
 {
     put_str("   idt_init start\n");
-    idt_desc_init();
+    idt_desc_init(); // init the idt description
+    // registing the interrupt handler
+    // and init the name of exception
+    exception_init(); 
+    // init the 8259A
     pic_init();
 
     uint64_t idt_operand = ((sizeof(idt) - 1) | ((uint64_t)((uint32_t)idt << 16)));
