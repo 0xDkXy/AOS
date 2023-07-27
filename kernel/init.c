2a081e28691ddda28424b7c6fa1a05d1bf21d8bc
0xDkXy <bo001015@outlook.com>
2023-07-27 23:07:14 +0800

    feat: complete timer

--- kernel/init.c
+++ kernel/init.c
@@ -2,10 +2,12 @@
 #include "kernel/print.h"
 #include "memory.h"
 #include "interrupt.h"
+#include "../device/timer.h"
 
 void init_all()
 {
     put_str("init_all\n");
     idt_init();
+    timer_init();
     mem_init();
 }
