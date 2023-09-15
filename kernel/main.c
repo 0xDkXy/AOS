30b1aa4b3ad95d283e967098a89583404203cf45
0xDkXy <0xdkxy.pollardlee@gmail.com>
2023-09-16 03:39:19 +0800

    feat: malloc and free, but there remain bugs

--- kernel/main.c
+++ kernel/main.c
@@ -37,6 +37,8 @@ int main(void)
     // console_put_int(sys_getpid());
     // console_put_str("\n");
 
+    process_execute(u_prog_a, "user_prog_a");
+    process_execute(u_prog_b, "user_prog_b");
     thread_start("k_thread_a", 31, k_thread_a, "I am thread_a");
     thread_start("k_thread_b", 31, k_thread_b, "I am thread_b");
 
@@ -46,115 +48,70 @@ int main(void)
 
 void k_thread_a(void* arg)
 {
-    char* para = arg;
-    void* addr1;
-    void* addr2;
-    void* addr3;
-    void* addr4;
-    void* addr5;
-    void* addr6;
-    void* addr7;
-    console_put_str("thread_a start\n");
-    int max = 1000;
-    while (max-- > 0) {
-        int size = 128;
-        addr1 = sys_malloc(size);
-        size *= 2;
-        addr2 = sys_malloc(size);
-        size *= 2;
-        addr3 = sys_malloc(size);
-        sys_free(addr1);
-        addr4 = sys_malloc(size);
-        size *= 2;
-        size *= 2;
-        size *= 2;
-        size *= 2;
-        size *= 2;
-        size *= 2;
-        size *= 2;
-        addr5 = sys_malloc(size);
-        addr6 = sys_malloc(size);
-        sys_free(addr5);
-        size *= 2;
-        addr7 = sys_malloc(size);
-        sys_free(addr2);
-        sys_free(addr3);
-        sys_free(addr4);
-        sys_free(addr6);
-        sys_free(addr7);
-    }
-    console_put_str("thread_a end\n");
+    void* addr1 = sys_malloc(256);
+    void* addr2 = sys_malloc(255);
+    void* addr3 = sys_malloc(254);
+    console_put_str("thread_a malloc addr: 0x");
+    console_put_int((uint32_t)addr1);
+    console_put_char(',');
+    console_put_int((uint32_t)addr2);
+    console_put_char(',');
+    console_put_int((uint32_t)addr3);
+    console_put_char('\n');
+
+    int cpu_delay = 1000000;
+    while (cpu_delay-- > 0);
+    sys_free(addr1);
+    sys_free(addr2);
+    sys_free(addr3);
     while(1);
 }
 
 void k_thread_b(void* arg)
 {
-    char* para = arg;
-    void* addr1;
-    void* addr2;
-    void* addr3;
-    void* addr4;
-    void* addr5;
-    void* addr6;
-    void* addr7;
-    void* addr8;
-    void* addr9;
-    console_put_str("thread_b start\n");
-    int max = 1000;
-    while (max-- > 0) {
-        int size = 9;
-        addr1 = sys_malloc(size);
-        size *= 2;
-        addr2 = sys_malloc(size);
-        size *= 2;
-        sys_free(addr2);
-        addr3 = sys_malloc(size);
-        sys_free(addr1);
-        addr4 = sys_malloc(size);
-        addr5 = sys_malloc(size);
-        addr6 = sys_malloc(size);
-        sys_free(addr5);
-        size *= 2;
-        addr7 = sys_malloc(size);
-        sys_free(addr3);
-        sys_free(addr4);
-        sys_free(addr6);
-        sys_free(addr7);
+    void* addr1 = sys_malloc(256);
+    void* addr2 = sys_malloc(255);
+    void* addr3 = sys_malloc(254);
+    console_put_str("thread_b malloc addr: 0x");
+    console_put_int((uint32_t)addr1);
+    console_put_char(',');
+    console_put_int((uint32_t)addr2);
+    console_put_char(',');
+    console_put_int((uint32_t)addr3);
+    console_put_char('\n');
 
-        size *= 2;
-        size *= 2;
-        size *= 2;
-        addr1 = sys_malloc(size);
-        addr2 = sys_malloc(size);
-        addr3 = sys_malloc(size);
-        addr4 = sys_malloc(size);
-        addr5 = sys_malloc(size);
-        addr6 = sys_malloc(size);
-        addr7 = sys_malloc(size);
-        addr8 = sys_malloc(size);
-        addr9 = sys_malloc(size);
-        sys_free(addr1);
-        sys_free(addr2);
-        sys_free(addr3);
-        sys_free(addr4);
-        sys_free(addr5);
-        sys_free(addr6);
-        sys_free(addr7);
-        sys_free(addr8);
-        sys_free(addr9);
-    }
-    console_put_str("thread_b end\n");
+    int cpu_delay = 1000000;
+    while (cpu_delay-- > 0);
+    sys_free(addr1);
+    sys_free(addr2);
+    sys_free(addr3);
     while(1);
 }
 
 void u_prog_a(void)
 {
-    printf("prog_a_pid: 0x%x\n", getpid());
-    while(1);
+    void* addr1 = malloc(256);
+    void* addr2 = malloc(255);
+    void* addr3 = malloc(254);
+    printf("prog_a malloc addr: 0x%x, 0x%x, 0x%x\n", (uint32_t)addr1,(uint32_t)addr2,(uint32_t)addr3);
+
+    int cpu_delay = 100000;
+    while (cpu_delay-- > 0);
+    free(addr1);
+    free(addr2);
+    free(addr3);
 }
 
 void u_prog_b(void)
 {
-    printf("prog_b_pid: 0x%x\n", getpid());
-    while(1);
+    void* addr1 = malloc(256);
+    void* addr2 = malloc(255);
+    void* addr3 = malloc(254);
+    printf("prog_a malloc addr: 0x%x, 0x%x, 0x%x\n", (uint32_t)addr1,(uint32_t)addr2,(uint32_t)addr3);
+
+    int cpu_delay = 100000;
+    while (cpu_delay-- > 0);
+    free(addr1);
+    free(addr2);
+    free(addr3);
 }
