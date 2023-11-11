a1cb5e4c1f53d29ec633f32b3c5ee6ff97551261
0xDkXy <0xdkxy.pollardlee@gmail.com>
2023-11-12 06:35:45 +0800

    feat: delete file

--- kernel/main.c
+++ kernel/main.c
@@ -56,27 +56,29 @@ int main(void)
     // printk("%d closed now\n", fd);
 
 
-    uint32_t fd = sys_open("/file2", O_RDWR);
-    printf("open /file2, fd: %d\n", fd);
-    char buf[64] = {0};
-    int read_bytes = sys_read(fd, buf, 18);
-    printf("1_ read %d bytes:\n%s\n", read_bytes, buf);
+    // uint32_t fd = sys_open("/file2", O_RDWR);
+    // printf("open /file2, fd: %d\n", fd);
+    // char buf[64] = {0};
+    // int read_bytes = sys_read(fd, buf, 18);
+    // printf("1_ read %d bytes:\n%s\n", read_bytes, buf);
 
-    memset(buf, 0, 64);
-    read_bytes = sys_read(fd, buf, 6);
-    printf("2_ read %d bytes:\n%s\n", read_bytes, buf);
+    // memset(buf, 0, 64);
+    // read_bytes = sys_read(fd, buf, 6);
+    // printf("2_ read %d bytes:\n%s\n", read_bytes, buf);
 
-    memset(buf, 0, 64);
-    read_bytes = sys_read(fd, buf, 6);
-    printf("3_ read %d bytes:\n%s\n", read_bytes, buf);
+    // memset(buf, 0, 64);
+    // read_bytes = sys_read(fd, buf, 6);
+    // printf("3_ read %d bytes:\n%s\n", read_bytes, buf);
 
-    printf("____ SEEK_SET 0 ----\n");
-    sys_lseek(fd, 0 ,SEEK_SET);
-    memset(buf, 0 ,64);
-    read_bytes = sys_read(fd, buf, 24);
-    printf("4_ read %d bytes:\n%s\n", read_bytes, buf);
+    // printf("____ SEEK_SET 0 ----\n");
+    // sys_lseek(fd, 0 ,SEEK_SET);
+    // memset(buf, 0 ,64);
+    // read_bytes = sys_read(fd, buf, 24);
+    // printf("4_ read %d bytes:\n%s\n", read_bytes, buf);
 
-    sys_close(fd);
+    // sys_close(fd);
+
+    printf("/file2 delete %s!\n", sys_unlink("/file2") == 0 ? "done" : "fail");
 
     while(1);
     return 0;
